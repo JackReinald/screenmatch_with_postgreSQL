7c996ef7d2a4fe39c8b608f92a263af275135891
JackReinald <83184806+JackReinald@users.noreply.github.com>
2024-07-04 11:47:53 -0500

    Update ConsultaChatGPT.java

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -8,7 +8,7 @@ public class ConsultaChatGPT {
     una cuenta paga, añadiendo la info. de la tarjeta, por lo tanto será descartado su uso.
      */
     public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService("sk-proj-tf70nTjxcIjAf3wUJ9CjT3BlbkFJaSeb2HFfRqAppbSdk8KU"); //OpenAO API key
+        OpenAiService service = new OpenAiService("API-KEY"); //OpenAO API key
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
